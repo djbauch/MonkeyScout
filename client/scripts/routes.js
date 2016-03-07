4f7c5674d1bc0cf5eabc1b1be8b160225e28645a
djbauch <dj.bauch@gmail.com>
2016-03-07 01:33:29 -0600

    Tabs beginning to work

--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -1,95 +1,105 @@
 angular.module('Scout.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
 
-  
-  $stateProvider
-  .state('autonomous', {
-    url: '/autonomous',
-    templateUrl: 'client/templates/autonomous.html',
-    controller: 'autonomousCtrl'
+
+    $stateProvider
+
+  .state('tabs', {
+      url: '/tabs',
+      templateUrl: 'client/templates/tabs.html',
+      abstract: true
   })
 
-  .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'client/templates/tabs.html'
+  .state('tabs.autonomous', {
+      url: '/autonomous',
+      views: {
+          'tab-autonomous': {
+              templateUrl: 'client/templates/autonomous.html',
+              controller: 'autonomousCtrl'
+          }
+      }
   })
 
-  .state('chats', {
-    url: '/chats',
-        templateUrl: 'client/templates/chats.html'
-  })
-  
-  .state('setup', {
-    url: '/setup',
-    templateUrl: 'client/templates/setup.html',
-    controller: 'setupCtrl'
+  .state('tabs.chats', {
+      url: '/chats',
+      templateUrl: 'client/templates/chats.html'
   })
 
-
-  .state('cloud', {
-    url: '/cloud',
-    templateUrl: 'client/templates/cloud.html',
-      controller: 'cloudCtrl'
-
+  .state('tabs.setup', {
+      url: '/setup',
+      views: {
+          'tab-setup': {
+              templateUrl: 'client/templates/setup.html',
+              controller: 'setupCtrl'
+          }
+      }
   })
 
-  .state('tabsController', {
-    url: '/page1',
-    templateUrl: 'client/templates/tabs.html',
-    abstract:true
+
+  .state('tabs.cloud', {
+      url: '/cloud',
+      views: {
+          'tab-cloud': {
+              templateUrl: 'client/templates/cloud.html',
+              controller: 'cloudCtrl'
+          }
+      }
   })
 
-  .state('teleOp', {
-    url: '/teleop',
-        templateUrl: 'client/templates/teleOp.html',
-        controller: 'teleOpCtrl'
 
+  .state('tabs.teleop', {
+      url: '/teleop',
+      views: {
+          'tab-teleop': {
+              templateUrl: 'client/templates/teleOp.html',
+              controller: 'teleOpCtrl'
+          }
+      }
   })
 
-  .state('match', {
-    url: '/match',
-        templateUrl: 'client/templates/match.html',
-        controller: 'matchCtrl'
+  .state('tabs.match', {
+      url: '/match',
+      templateUrl: 'client/templates/match.html',
+      controller: 'matchCtrl'
   })
 
-  .state('pitScouting', {
-    url: '/pit',
-         templateUrl: 'client/templates/pitScouting.html',
-        controller: 'pitScoutingCtrl'
-   
+  .state('tabs.pit', {
+      url: '/pit',
+      templateUrl: 'client/templates/pitScouting.html',
+      controller: 'pitScoutingCtrl'
+
   })
 
   .state('tab7DefaultPage', {
-    url: '/page8',
-    templateUrl: 'client/templates/tab7DefaultPage.html',
-    controller: 'tab7DefaultPageCtrl'
+      url: '/page8',
+      templateUrl: 'client/templates/tab7DefaultPage.html',
+      controller: 'tab7DefaultPageCtrl'
   })
 
-  .state('tabsController.configuration', {
-    url: '/configure',
-        templateUrl: 'client/templates/configuration.html',
-        controller: 'configurationCtrl'
+  .state('tabs.configure', {
+      url: '/configure',
+      templateUrl: 'client/templates/configuration.html',
+      controller: 'configurationCtrl'
   })
 
   .state('login', {
-    url: '/page10',
-    templateUrl: 'client/templates/login.html',
-    controller: 'loginCtrl'
+      url: '/page10',
+      templateUrl: 'client/templates/login.html',
+      controller: 'loginCtrl'
   })
 
   .state('signup', {
-    url: '/page11',
-    templateUrl: 'client/templates/signup.html',
-    controller: 'signupCtrl'
+      url: '/page11',
+      templateUrl: 'client/templates/signup.html',
+      controller: 'signupCtrl'
   });
 
-  $urlRouterProvider.otherwise('/autonomous');
+    $urlRouterProvider.otherwise('/tabs/setup');
 
 });
